2b5c1938dd9123ded8a30547bdc9a9a7914a53bf
YoonJiWon0323 <144296387+YoonJiWon0323@users.noreply.github.com>
2026-03-07 14:34:09 +0900

    mirroring_chatbot_f.py

--- streamlit-app/mirroring_chatbot_f.py
+++ streamlit-app/mirroring_chatbot_f.py
@@ -663,34 +663,7 @@ elif st.session_state.phase == "conversation":
         # ---------------- 종료 질문 이후 처리 ----------------
         if st.session_state.end_confirm and not st.session_state.refund_confirm:
 
-            finish_intent = detect_refund_finish_intent(user_input)
-
-            # 종료 의사
-            if finish_intent:
-
-                st.session_state.refund_confirm = True
-
-                if st.session_state.tone == "격식체":
-                    msg = "환불 심사 진행 요청으로 이해해도 되겠습니까?"
-                elif st.session_state.tone == "해요체":
-                    msg = "환불 심사를 진행해 달라는 요청으로 이해해도 될까요?"
-                else:
-                    msg = "환불 심사 진행 요청으로 이해해도 될까?"
-
-                st.session_state.chat_log.append(("assistant", msg))
-                st.chat_message("assistant").write(msg)
-
-                st.stop()
-
-            # 종료 안함 → 상담 계속
-            else:
-                st.session_state.end_confirm = False
-
-
-        # ---------------- 심사 요청 최종 확인 ----------------
-        if st.session_state.refund_confirm:
-
-            simple_yes = ["응", "네", "예", "그래", "ㅇㅋ", "ok", "yes"]
+            simple_yes = ["응","네","예","그래","ㅇㅋ","ok","yes"]
 
             if user_input.strip().lower() in simple_yes:
                 finish_intent = True
@@ -713,18 +686,19 @@ elif st.session_state.phase == "conversation":
 
                 st.stop()
 
+
     # ---------------- 여행지 추천 시나리오 ----------------
     elif st.session_state.scenario == "recommend":
 
         # ---------------- 종료 질문 이후 처리 ----------------
-        if st.session_state.end_confirm and not st.session_state.recommend_confirm:
+        if st.session_state.end_confirm and not st.session_state.refund_confirm:
 
-            simple_yes = ["응", "네", "예", "그래", "ㅇㅋ", "ok", "yes"]
+            simple_yes = ["응","네","예","그래","ㅇㅋ","ok","yes"]
 
             if user_input.strip().lower() in simple_yes:
                 finish_intent = True
             else:
-                finish_intent = detect_recommend_finish_intent(user_input)
+                finish_intent = detect_refund_finish_intent(user_input)
 
             if finish_intent:
 
